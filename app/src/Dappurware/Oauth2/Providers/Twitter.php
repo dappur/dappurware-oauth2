28cf75254e5e2f24a5b79fd72008d3d50c803d96
Edward Teach <jeffmthomas@gmail.com>
2020-01-13 14:30:35 -0800

    Update Twitter.php

--- app/src/Dappurware/Oauth2/Providers/Twitter.php
+++ app/src/Dappurware/Oauth2/Providers/Twitter.php
@@ -2,7 +2,7 @@
 
 namespace Dappur\Dappurware\Oauth2\Providers;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 
 class Twitter extends \Dappur\Dappurware\Oauth2
 {
